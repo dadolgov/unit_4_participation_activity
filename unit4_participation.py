907d314f2628f2938d23d4c9a50225ec2af154d8
Dmitrii Dolgov <ddolgov1@student.cscc.edu>
2026-02-07 20:20:40 -0500

    Removed the list output, since it wasn't required in the task

--- unit4_participation.py
+++ unit4_participation.py
@@ -8,8 +8,8 @@ print out the total number or elements, sum and average
 import random
 #Creating a new list of random integers using list comprehension
 random_numbers=[random.randint(1,10) for _ in range(100)]
+
 #printing length, sum and average respectively
-print(f"Here's the list of 100 random numbers: {random_numbers}")
 print(f"Total elements in the list: {len(random_numbers)}")
 print(f"Sum of all elements is {sum(random_numbers)}")
 print(f"Average of all values is {sum(random_numbers)/len(random_numbers):.2f}") #limited to 2 decimal spaces for better readability
